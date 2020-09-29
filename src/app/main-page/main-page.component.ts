4d4077956b0f1f35ddf824b9e436b4f6e5ff548c
unknown <cyprian_kalina@wp.pl>
2020-09-29 21:12:31 +0200

    Added button and a scrpipt in main-page component. It can change the theme of the website from dark to bright

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -11,8 +11,31 @@ export class MainPageComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  
   goToBottom(){
     window.scrollTo(-300,document.body.scrollHeight);
+  }
+
+  changeColor(){
+    if(document.body.className=="jasny"){
+      document.body.className = document.body.className.replace("jasny","");
+      document.getElementById('kontener').className = "";
+      document.getElementById('opcjagoraprawo').className = "";
+      document.getElementById('opcjadollewo').className = "";
+      document.getElementById('opcjadolprawo').className = "";
+      document.getElementById('opcjagoralewo').className = "";
+      document.getElementById('buttoncolor').style.backgroundColor="#30214d";
+      document.getElementById('buttoncolor').style.color="#64567e";
+    }
+    else{
+      document.getElementById('buttoncolor').style.backgroundColor="#4e3b74";
+      document.getElementById('buttoncolor').style.color="#796e8d";
+      document.body.className = "jasny";
+      document.getElementById('kontener').className = "jasny";
+      document.getElementById('opcjagoraprawo').className = "jasnyprzycisk1";
+      document.getElementById('opcjadollewo').className = "jasnyprzycisk1";
+      document.getElementById('opcjadolprawo').className = "jasnyprzycisk2";
+      document.getElementById('opcjagoralewo').className = "jasnyprzycisk2";
+    }
   }
 }
