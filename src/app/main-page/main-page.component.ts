e3f0d5460eea69736563912babf849735d75583c
unknown <cyprian_kalina@wp.pl>
2020-10-04 15:19:11 +0200

    Finalizing of omnie-component

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -16,33 +16,34 @@ export class MainPageComponent implements OnInit {
     window.scrollTo(-300,document.body.scrollHeight);
   }
 
-  changeColor(){
-    if(document.body.className=="jasny"){
+  changeColor(x:string){
+    if(x=="ciemny"){
       document.body.className = document.body.className.replace("jasny","");
       document.getElementById('kontener').className = "";
       document.getElementById('opcjagoraprawo').className = "";
       document.getElementById('opcjadollewo').className = "";
       document.getElementById('opcjadolprawo').className = "";
       document.getElementById('opcjagoralewo').className = "";
-      document.getElementById('ciemnastrona').style.opacity="1.0";
-      document.getElementById('jasnastrona').style.opacity="0.1";
-      document.getElementById('zmien').style.color="white";
-      document.getElementById('kolor').style.color="black";
+      document.getElementById('kontenerr').className="";
+      document.getElementById('krocej').className="";
+      document.getElementById('dluzej').className="";
+      document.getElementById('krocej').style.color="#6a4ba3";  
+      document.getElementById('pe').className="";
+      document.getElementById('pepe').className="";
     }
     else{
-
       document.body.className = "jasny";
-      document.getElementById('zmien').style.color="black";
-      document.getElementById('kolor').style.color="white";
-
-      document.getElementById('buttoncolor').style.color="white";
-      document.getElementById('ciemnastrona').style.opacity="0.3";
-      document.getElementById('jasnastrona').style.opacity="1.0";
       document.getElementById('kontener').className = "jasny";
       document.getElementById('opcjagoraprawo').className = "jasnyprzycisk1";
       document.getElementById('opcjadollewo').className = "jasnyprzycisk1";
       document.getElementById('opcjadolprawo').className = "jasnyprzycisk2";
       document.getElementById('opcjagoralewo').className = "jasnyprzycisk2";
+      document.getElementById('kontenerr').className="jasnydr";
+      document.getElementById('krocej').className="jasnyopis";
+      document.getElementById('dluzej').className="jasnyopis";
+      document.getElementById('krocej').style.color="rgb(226, 220, 220)"    
+      document.getElementById('pe').className="jasnep";
+      document.getElementById('pepe').className="jasnep";
     }
   }
 }
