9ae49cbd44632b3304f9d7ba8133f487d1febd2f
unknown <cyprian_kalina@wp.pl>
2020-10-05 21:56:25 +0200

    Added another component
    -Added kontakt-component and basic css for it
    -Changed the css of omnie-component, it  is probably a final version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { KontaktComponent } from './kontakt/kontakt.component';
 import { PlaceHolderComponent } from './place-holder/place-holder.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -6,7 +7,8 @@ import { OmnieComponent } from './omnie/omnie.component';
 
 const routes: Routes = [
   {path:'placeholder', component:PlaceHolderComponent},
-  {path:'omnie',component:OmnieComponent}
+  {path:'omnie',component:OmnieComponent},
+  {path:'kontakt',component:KontaktComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { PlaceHolderComponent } from './place-holder/place-holder.component';
 import { OmnieComponent } from './omnie/omnie.component';
+import { KontaktComponent } from './kontakt/kontakt.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     PlaceHolderComponent,
-    OmnieComponent
+    OmnieComponent,
+    KontaktComponent
   ],
   imports: [
     BrowserModule,
