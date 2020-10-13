9ce6cec638ca0f93586590de0abd768ef68b3418
unknown <cyprian_kalina@wp.pl>
2020-10-13 23:18:33 +0200

    Modified functionality in achive-component - added skeleton for function that will pop up description of clicked element

--- src/app/achive-component/achive-component.component.ts
+++ src/app/achive-component/achive-component.component.ts
@@ -9,7 +9,35 @@ export class AchiveComponentComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  goToBottom(){
+    window.scrollTo(-100,document.body.scrollHeight);
+  }
+  ngOnInit() {
+    setTimeout(this.goToBottom, 500);
   }
 
+  wysun(x:number){
+    
+    if (x>0){
+   
+      document.getElementById("slajd").style.width="80%";
+      document.getElementById("slajd").style.height="auto";
+      document.getElementById("slajd").style.marginLeft="10%";
+    
+
+      const ha = document.getElementById("slajdtytul");
+      const pe = document.getElementById("slajdp");
+      if (x==1){
+        ha.innerText="To portfolio...";
+        pe.innerText="To portfolio to mój pierwszy większy projekt w Angulrze. Jestem z niego dumny - zrobiłem go w 100% sam, i wykorzystałem bardzo wiele nowych dla mnie rozwiązań które pomogą mi w dalszym rozwoju ";
+      }
+ 
+    }
+    else if (x==0){
+      document.getElementById("slajd").style.width="0%";
+      document.getElementById("slajd").style.height="-1px";
+
+      
+    }
+  }
 }
