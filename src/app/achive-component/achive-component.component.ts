b6da3f5d00a8ec06b5ccd6cf8748ef7b7b668133
unknown <cyprian_kalina@wp.pl>
2020-10-14 19:24:23 +0200

    -Modified functionality of achive-component, changes in function "wysun()"

--- src/app/achive-component/achive-component.component.ts
+++ src/app/achive-component/achive-component.component.ts
@@ -19,11 +19,12 @@ export class AchiveComponentComponent implements OnInit {
   wysun(x:number){
     
     if (x>0){
-   
+      document.getElementById("punkt").scrollIntoView({behavior: 'smooth'});     
       document.getElementById("slajd").style.width="80%";
       document.getElementById("slajd").style.height="auto";
       document.getElementById("slajd").style.marginLeft="10%";
-    
+      
+      
 
       const ha = document.getElementById("slajdtytul");
       const pe = document.getElementById("slajdp");
@@ -31,11 +32,17 @@ export class AchiveComponentComponent implements OnInit {
         ha.innerText="To portfolio...";
         pe.innerText="To portfolio to mój pierwszy większy projekt w Angulrze. Jestem z niego dumny - zrobiłem go w 100% sam, i wykorzystałem bardzo wiele nowych dla mnie rozwiązań które pomogą mi w dalszym rozwoju ";
       }
+      else if (x==5){
+        
+        document.getElementById("slajd").style.fontSize="30px";
+        ha.innerText="Witryna cukierni...";
+        pe.innerText="To bardzo prosty projekt wykonany w ramach zadań szkolnych. Wyróżnia go jednak to że postanowiłem się tu postawić nauczycielowi, i zamiast pisać sam nudny html i css użyłem jeszcze po raz pierwszy FlexBoxa - i wyszło mi nawet nieźle! ";
+      }
  
     }
     else if (x==0){
       document.getElementById("slajd").style.width="0%";
-      document.getElementById("slajd").style.height="-1px";
+      
 
       
     }
